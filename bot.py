c59a74c2dd2a0acdb33a563e21a99b77560f6cf9
CmetaStoned <mendbalprincess87@gmail.com>
2026-04-01 17:01:28 +0300

    BSI

--- bot.py
+++ bot.py
@@ -2,14 +2,16 @@ import os
 import asyncio
 import traceback
 from zoneinfo import ZoneInfo
-from aiohttp import web  # Добавили импорт для веб-заглушки
+from aiohttp import web  
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import ReplyKeyboardRemove, Message
 from datetime import datetime, timedelta
 from scripts.code10 import load_env_with_password
 from scripts.code20 import encrypt_and_store_entry
+import base64
+from io import BytesIO
 
-# Достаем токен из переменных окружения сервера
+
 TOKEN = os.getenv("BOT_TOKEN")
 
 if not TOKEN:
@@ -26,30 +28,30 @@ async def handle_ping(request):
     return web.Response(text="Бот дневника работает 24/7!")
 
 
-
-@dp.message(F.text)
+@dp.message(F.text | F.photo)
 async def handle_message(message: Message):
     user_id = message.from_user.id
     now = datetime.now(ZoneInfo("Asia/Jerusalem"))
 
-    # Если ждём пароль
+    # ЕСЛИ ЖДЕМ ПАРОЛЬ
     if user_id in user_data and "entry" in user_data[user_id]:
         entry = user_data[user_id]["entry"]
         timestamp = user_data[user_id]["timestamp"]
-
+        img = user_data[user_id].get("img")  
         if user_id in user_passwords and now - user_passwords[user_id]["time"] < timedelta(hours=3):
             password = user_passwords[user_id]["password"]
         else:
             password = message.text
             user_passwords[user_id] = {"password": password, "time": now}
 
-        # ЗАЩИТА ОТ КРИВОГО ПАРОЛЯ
         try:
             env = load_env_with_password(str(password))
-            encrypt_and_store_entry(timestamp, entry, env)
+            
+            safe_entry = str(entry) if entry else ""
+            encrypt_and_store_entry(timestamp, safe_entry, img, env) 
         except Exception as e:
             print(f"Ошибка расшифровки: {e}")
-            user_passwords.pop(user_id, None) # Удаляем неверный пароль из памяти
+            user_passwords.pop(user_id, None)
             await message.answer("Неверный пароль")
             user_data.pop(user_id, None)
             return
@@ -66,34 +68,46 @@ async def handle_message(message: Message):
         await message.answer(f"Запись сохранена: <b>{timestamp}</b>", parse_mode="HTML", reply_markup=ReplyKeyboardRemove())
         user_data.pop(user_id)
 
+    # ЕСЛИ ЭТО НОВАЯ ЗАПИСЬ
     else:
-        entry = message.text
+        entry = message.text if message.text else message.caption 
+        img = None  
+        if message.photo:
+            img1 = message.photo[-1]
+            file_in_memory = BytesIO()
+            file_info = await bot.get_file(img1.file_id)
+            await bot.download_file(file_info.file_path, file_in_memory)
+            image_bytes = file_in_memory.getvalue()
+            base64_string = base64.b64encode(image_bytes).decode('utf-8')
+            img = f"data:image/jpeg;base64,{base64_string}"
+            
         timestamp = now.strftime("%d.%m.%Y %H:%M")
         await message.delete()
 
         if user_id in user_passwords and now - user_passwords[user_id]["time"] < timedelta(hours=3):
             password = user_passwords[user_id]["password"]
             
-            # ТУТ ТОЖЕ ЗАЩИТА ДЛЯ СОХРАНЕННОГО ПАРОЛЯ
             try:
                 env = load_env_with_password(password)
-                encrypt_and_store_entry(timestamp, str(entry), env)
+                safe_entry = str(entry) if entry else ""
+                encrypt_and_store_entry(timestamp, safe_entry, img, env)
                 print(f'Добавлена запись: [{timestamp}]')
                 await message.answer(f"Запись сохранена: <b>{timestamp}</b>", parse_mode="HTML", reply_markup=ReplyKeyboardRemove())
             except Exception as e:
                 print(f"Ошибка кэша: {e}")
                 user_passwords.pop(user_id, None)
-                await message.answer("❌ Сохраненный пароль слетел. Отправь запись заново.")
+                await message.answer("Сохраненный пароль слетел. Отправь запись заново.")
         else:
-            control_msg = await message.answer("Контроль")
+            control_msg = await message.answer("Введи пароль для сохранения:")
             user_data[user_id] = {
-                "entry": entry,
+                "entry": str(entry) if entry else "",  
                 "timestamp": timestamp,
+                "img": img,  
                 "control_msg_id": control_msg.message_id
             }
 
 async def main():
-    # --- НАСТРОЙКА И ЗАПУСК ВЕБ-ЗАГЛУШКИ ---
+    # НАСТРОЙКА И ЗАПУСК ЗАГЛУШКИ ДЛЯ ХОСТА 
     app = web.Application()
     app.router.add_get('/', handle_ping)
     
@@ -104,7 +118,7 @@ async def main():
     site = web.TCPSite(runner, '0.0.0.0', port)
     await site.start()
     print(f"Веб-заглушка запущена на порту {port}")
-    # --- КОНЕЦ НАСТРОЙКИ ---
+    # КОНЕЦ НАСТРОЙКИ 
 
     try:
         print("Бот запущен")
@@ -115,7 +129,7 @@ async def main():
         traceback.print_exc()
     finally:
         await bot.session.close()
-        await runner.cleanup()  # Важно: закрываем веб-сервер при выключении бота
+        await runner.cleanup()  # закрываем веб-сервер при выключении бота
         print("Бот остановлен")
 
 if __name__ == "__main__":
--- scripts/code20.py
+++ scripts/code20.py
@@ -1,16 +1,20 @@
 from cryptography.fernet import Fernet
 import psycopg2
 
-def encrypt_and_store_entry(dateantime: str, entry: str, env):
+def encrypt_and_store_entry(dateantime: str, entry: str, img: str | None, env):
     key = env["FERNET_KEY"]
     fernet = Fernet(key.encode())
     encrypted_date = fernet.encrypt(dateantime.encode())
     encrypted_entry = fernet.encrypt(entry.encode())
-
+    if img is not None:
+            encrypted_image = fernet.encrypt(img.encode())
+    else:
+        encrypted_image = None
     conn = psycopg2.connect(env['DATABASE_URL'])
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO entrys (dateandtime, entry) VALUES (%s, %s)", (encrypted_date, encrypted_entry))
+    cursor.execute("INSERT INTO entrys (dateandtime, entry, img) VALUES (%s, %s, %s)", (encrypted_date, encrypted_entry, encrypted_image))
     conn.commit()
+    cursor.close()
     conn.close()
 
 
@@ -19,12 +23,19 @@ def decrypt_entry(env):
     fernet = Fernet(key.encode())
     conn = psycopg2.connect(env['DATABASE_URL'])
     cursor = conn.cursor()
-    cursor.execute("SELECT id, dateandtime, entry FROM entrys")
+    cursor.execute("SELECT id, dateandtime, entry, img FROM entrys")
     rows = cursor.fetchall()
-    dick = [{"id": ide ,"dateandtime": fernet.decrypt(bytes(date)).decode(),
-              "entry": fernet.decrypt(bytes(entry)).decode()}
-                for ide, date, entry 
-                in rows]
+    dick = []
+    for ide, date, entry, img in rows:
+        decrypted_image = fernet.decrypt(bytes(img)).decode() if img is not None else None
+        
+        dick.append({
+            "id": ide,
+            "dateandtime": fernet.decrypt(bytes(date)).decode(),
+            "entry": fernet.decrypt(bytes(entry)).decode(),
+            "image": decrypted_image
+        })
+        
     conn.close()
     return dick
 
